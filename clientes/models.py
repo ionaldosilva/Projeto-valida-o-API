681332ef20d0feff8029983ef4a5e069c8282da1
Ionaldo Silva <ionaldo_araujo@hotmail.com>
2023-08-01 22:31:43 -0300

    inicio validações

--- clientes/models.py
+++ clientes/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
-    email = models.EmailField(blank=False, max_length=30, )
-    cpf = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(blank=False, max_length=30, unique=True)
+    cpf = models.CharField(max_length=11, blank=False, null=False, unique=True)
     rg = models.CharField(max_length=9)
     celular = models.CharField(max_length=14)
     ativo = models.BooleanField()
